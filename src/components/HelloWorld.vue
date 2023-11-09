b4935cc1d067ac7ca6ecae2a0f290c2f138aa437
CodeSandbox <support@codesandbox.io>
2023-11-09 18:04:15 +0000

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,56 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-defineProps<{ msg: string }>();
+const messageText = ref(0);
 
-const count = ref(0);
+const plusPlus = () => {
+  messageText.value++;
+};
+
+const minusMinus = () => {
+  if (messageText.value > 0) {
+    messageText.value--;
+  }
+};
+
+const resettey = () => {
+  messageText.value = 0;
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ messageText }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button
+      type="button"
+      @click="
+        (event) => {
+          if (messageText > 0) {
+            messageText = messageText - 1;
+          }
+        }
+      "
+    >
+      Minus me!
+    </button>
+    <button type="button" @click="messageText = 0">Reset me!</button>
+    <button type="button" @click="messageText = messageText + 1">
+      Plus me!
+    </button>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="card">
+    <button type="button" @click="minusMinus">
+      Minus me but with a function!
+    </button>
+    <button type="button" @click="resettey">
+      Reset me but with a function!
+    </button>
+    <button type="button" @click="plusPlus">
+      Plus me but with a function!
+    </button>
+  </div>
 </template>
 
 <style scoped>
